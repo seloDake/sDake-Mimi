dea9a0a5ccca2b2c5c7492bb9a6c819009d7e8d9
Selorm Dake <123432641+seloDake@users.noreply.github.com>
2023-02-01 01:30:46 -0500

    Took a look at results

--- Tutorial2.jl
+++ Tutorial2.jl
@@ -1,6 +1,14 @@
 #Tutorial 2 File
+using Mimi
 using MimiFUND
+
 
 #runs FUND model
 m = MimiFUND.get_model()
 run(m)
+
+# Take a lot at results
+m[:socioeconomic, :income] # complete array
+m[:socioeconomic, :income][100] # individual index
+getdataframe(m, :socioeconomic=>:income) # dataframe requesting one variable from one component
+getdataframe(m, :socioeconomic=>:income)[1:16,:]# results for regions in 950
