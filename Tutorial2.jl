901a001eec1d673fc7f205d8de4bf6d9c5735592
Selorm Dake <123432641+seloDake@users.noreply.github.com>
2023-02-01 01:53:21 -0500

    I completed Tutorial 2 also saved picture of plot.

--- Tutorial2.jl
+++ Tutorial2.jl
@@ -12,3 +12,11 @@ m[:socioeconomic, :income] # complete array
 m[:socioeconomic, :income][100] # individual index
 getdataframe(m, :socioeconomic=>:income) # dataframe requesting one variable from one component
 getdataframe(m, :socioeconomic=>:income)[1:16,:]# results for regions in 950
+
+
+# Explore Plots and Graphs
+explore(m) #creates second window with graphs
+
+# How to save plots
+plott = Mimi.plot( m, :socioeconomic, :income)
+save("MyFilePath.svg", plott)
